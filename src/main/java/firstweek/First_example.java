c185ed7b19506e328b862417e3eb393ef8cfd3b5
devnarayanprasad <devnarayanprasad@gmail.com>
2019-12-17 22:40:10 +0530

    First Class

---
+++ src/main/java/firstweek/First_example.java
@@ -0,0 +1,12 @@
+package firstweek;
+
+public class First_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("bhello");
+
+	}
+
+}
